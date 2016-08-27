07e10edd3096f1b06cc0176215e56f13a84d427c
Elia Lombardi <elia.lombardi@outlook.it>
2016-08-27 08:04:09 +0200

    Added Passport JWT

--- index.js
+++ index.js
@@ -20,7 +20,18 @@ db.once('open', () => console.log(`Database connected`));
 // Express config
 const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
+const passport = require('passport');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const jwtOptions = {
+    jwtFromRequest: ExtractJwt.fromAuthHeader(),
+    secretOrKey: 'secret',
+    algorithms: ['HS256']
+}
 
+passport.use(new JwtStrategy(jwtOptions, (jwtPayload, done) => done(null, { name: jwtPayload.name }) ));
+
+app.use(passport.authenticate('jwt', { session: false }))
 app.use('/', bodyParser.json());
 
 
